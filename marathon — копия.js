7ac24044d793e1532b80d4bcb4e710d2238b84fe
Sabyrzhan Islam <109369245+Sabyrzhan23@users.noreply.github.com>
2023-01-12 15:14:31 +0500

    kartochek
    kartochek

---
+++ marathon — копия.js
@@ -0,0 +1,25 @@
+function slidesAll(activeSlide = 4) {
+    const slides = document.querySelectorAll('.slide')
+    slides[activeSlide].classList.add('active')
+    
+    
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+    
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    } 
+}
+slidesAll(0)
+
+
+
+
+
